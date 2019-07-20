3b355f26f32b478c02bec8145d715c3f17129755
Jack Baron <jackmbaron@gmail.com>
2019-07-20 05:24:19 +0100

    rip out old typedefs

--- src/difficulty.go
+++ src/difficulty.go
@@ -7,86 +7,6 @@ import (
 	"github.com/TomOnTime/utfutil"
 )
 
-// OldDifficultyJSON is the old beatmap difficulty file
-type OldDifficultyJSON struct {
-	Version string `json:"_version"`
-
-	BeatsPerMinute float64 `json:"_beatsPerMinute"`
-	BeatsPerBar    int     `json:"_beatsPerBar"`
-
-	NoteJumpSpeed           float64 `json:"_noteJumpSpeed"`
-	NoteJumpStartBeatOffset int     `json:"_noteJumpStartBeatOffset"`
-
-	Shuffle       float64 `json:"_shuffle"`
-	ShufflePeriod float64 `json:"_shufflePeriod"`
-
-	ColorLeft  *BeatmapColor `json:"_colorLeft,omitempty"`
-	ColorRight *BeatmapColor `json:"_colorRight,omitempty"`
-
-	Time int `json:"_time"`
-
-	Warnings     []string `json:"_warnings"`
-	Information  []string `json:"_information"`
-	Suggestions  []string `json:"_suggestions"`
-	Requirements []string `json:"_requirements"`
-
-	BPMChanges []BPMChange `json:"_BPMChanges"`
-	Events     []Event     `json:"_events"`
-	Notes      []Note      `json:"_notes"`
-	Obstacles  []Obstacle  `json:"_obstacles"`
-	Bookmarks  []Bookmark  `json:"_bookmarks"`
-}
-
-// NewDifficultyJSON is the new beatmap difficulty file
-type NewDifficultyJSON struct {
-	Version string `json:"_version"`
-
-	BPMChanges []BPMChange `json:"_BPMChanges"`
-	Events     []Event     `json:"_events"`
-	Notes      []Note      `json:"_notes"`
-	Obstacles  []Obstacle  `json:"_obstacles"`
-	Bookmarks  []Bookmark  `json:"_bookmarks"`
-}
-
-// BPMChange MM BPM Change
-type BPMChange struct {
-	BPM             float64 `json:"_BPM"`
-	Time            float64 `json:"_time"`
-	BeatsPerBar     int     `json:"_beatsPerBar"`
-	MetronomeOffset int     `json:"_metronomeOffset"`
-}
-
-// Event Beatmap Event
-type Event struct {
-	Time  float64 `json:"_time"`
-	Type  int     `json:"_type"`
-	Value int     `json:"_value"`
-}
-
-// Note Beatmap Note
-type Note struct {
-	Time         float64 `json:"_time"`
-	LineIndex    int     `json:"_lineIndex"`
-	LineLayer    int     `json:"_lineLayer"`
-	Type         int     `json:"_type"`
-	CutDirection int     `json:"_cutDirection"`
-}
-
-// Obstacle Beatmap Obstacle
-type Obstacle struct {
-	Time      float64 `json:"_time"`
-	LineIndex int     `json:"_lineIndex"`
-	Type      int     `json:"_type"`
-	Duration  float64 `json:"_duration"`
-	Width     int     `json:"_width"`
-}
-
-// Bookmark MM Bookmark
-type Bookmark struct {
-	Time float64 `json:"_time"`
-	Name string  `json:"_name"`
-}
-
 func readDifficulty(path string) (OldDifficultyJSON, error) {
 	bytes, err := utfutil.ReadFile(path, utfutil.UTF8)
 	if err != nil {
@@ -103,20 +23,4 @@ func readDifficulty(path string) (OldDifficultyJSON, error) {
 	json.Unmarshal(bytes, &diffJSON)
 
 	return diffJSON, nil
-}
-
-func getRank(difficulty string) int {
-	if difficulty == "Easy" {
-		return 1
-	} else if difficulty == "Normal" {
-		return 3
-	} else if difficulty == "Hard" {
-		return 5
-	} else if difficulty == "Expert" {
-		return 7
-	} else if difficulty == "ExpertPlus" {
-		return 9
-	} else {
-		return 3
-	}
 }
--- src/oldInfo.go
+++ src/oldInfo.go
@@ -7,41 +7,6 @@ import (
 	"github.com/TomOnTime/utfutil"
 )
 
-// OldInfoJSON is the old beatmap info file
-type OldInfoJSON struct {
-	SongName    string `json:"songName"`
-	SongSubName string `json:"songSubName"`
-	AuthorName  string `json:"authorName"`
-
-	Contributors []struct {
-		Role     string `json:"role"`
-		Name     string `json:"name"`
-		IconPath string `json:"iconPath"`
-	} `json:"contributors"`
-
-	BeatsPerMinute        float64 `json:"beatsPerMinute"`
-	PreviewStartTime      float64 `json:"previewStartTime"`
-	PreviewDuration       float64 `json:"previewDuration"`
-	CoverImagePath        string  `json:"coverImagePath"`
-	EnvironmentName       string  `json:"environmentName"`
-	OneSaber              bool    `json:"oneSaber"`
-	CustomEnvironment     string  `json:"customEnvironment"`
-	CustomEnvironmentHash string  `json:"customEnvironmentHash"`
-
-	DifficultyLevels []struct {
-		Difficulty      string `json:"difficulty"`
-		DifficultyRank  int    `json:"difficultyRank"`
-		AudioPath       string `json:"audioPath"`
-		JSONPath        string `json:"jsonPath"`
-		Offset          int    `json:"offset"`
-		OldOffset       int    `json:"oldOffset"`
-		ChromaToggle    string `json:"chromaToggle"`
-		CustomColors    bool   `json:"customColors"`
-		Characteristic  string `json:"characteristic"`
-		DifficultyLabel string `json:"difficultyLabel"`
-	} `json:"difficultyLevels"`
-}
-
 func readInfo(path string) (OldInfoJSON, error) {
 	bytes, err := utfutil.ReadFile(path, utfutil.UTF8)
 	if err != nil {
