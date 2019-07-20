05a64143f3c65a8baf8c14475471d0b4b0d5097c
Jack Baron <jackmbaron@gmail.com>
2019-07-20 06:19:20 +0100

    add some more io helpers to check existence

--- utils/io.go
+++ utils/io.go
@@ -21,14 +21,59 @@ func CopyFile(src, dst string) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	defer source.Close()
 
+	defer source.Close()
 	destination, err := os.Create(dst)
 	if err != nil {
 		return 0, err
 	}
-	defer destination.Close()
 
+	defer destination.Close()
 	nBytes, err := io.Copy(destination, source)
 	return nBytes, err
+}
+
+// PathExists checks for existence of an item at path
+func PathExists(path string) (bool, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// FileExists checks for existence of a file at path
+func FileExists(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	mode := stat.Mode()
+	if mode.IsDir() {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// DirectoryExists checks for existence of a file at path
+func DirectoryExists(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	mode := stat.Mode()
+	if mode.IsRegular() {
+		return false, nil
+	}
+
+	return true, nil
 }
